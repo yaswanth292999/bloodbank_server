b624a7665b18992e2910b837494a70839e70a693
Yaswanth Ambarukhana <saiambharukhanarama1234@gmail.com>
2023-04-18 21:18:31 +0530

    added createDonor endpoint

--- donorRouter.js
+++ donorRouter.js
@@ -5,6 +5,15 @@ import  queryDb  from '../bloodbank_server/db.js';
 export const donorRouter=Router();
 
 donorRouter.get('/getDonorForBloodGroup',getDonorForBloodGroup)
+donorRouter.post('/createDonor',createDonor)
+
+
+async function createDonor(req,res){
+    const {name,blood_group,medical_report,address,contact_number,blood_bank,available_units}=req.body
+    const result=await queryDb('INSERT INTO donor (name, blood_group,medical_report,address,contact_number,blood_bank,available_units) VALUES($1,$2,$3,$4,$5,$6,$7)')
+    res.status(200).end(JSON.stringify(result.rows))
+}
+
 
 async function getDonorForBloodGroup(req,res){
     const {bloodbank,blood_group,required_units}=req.query
